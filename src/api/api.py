9c7ea0313df7e59d544641164d96ad7f0358050e
Sebastian009w <ia3266505@gmail.com>
2023-02-28 22:06:27 -0500

    documentacion

--- src/api/api.py
+++ src/api/api.py
@@ -5,6 +5,7 @@ from PIL import Image
 import ascii_magic
 import requests
 
+# Api
 URL_API = 'https://pokeapi.co/api/v2/pokemon/'
 POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=1000"
 IMAGE_FILE = 'pokemon.png'
@@ -13,11 +14,13 @@ IMAGE_FILE = 'pokemon.png'
 def get_pokemon_data(name):
     """GET /"""
     try:
+        # Peticion mas el nombre del pokemon
         url = f'{URL_API}{name.lower()}'
         response = requests.get(url, timeout=10)
         response.raise_for_status()
         return response.json()
     except (requests.exceptions.HTTPError, ValueError) as error_request:
+        # SI ocurre un error 
         print(
             f'error de la solicitud a la ruta: {url}: error: {error_request}')
         return None
@@ -26,8 +29,10 @@ def get_pokemon_data(name):
 def pokemon_image(pokemon__data):
     """image pokemon."""
     try:
+        # - Tomamos la foto del pokemon la parte front
         sprites__url = pokemon__data["sprites"]["front_default"]
         response = requests.get(sprites__url, timeout=10)
+        # - luego la psamos a binario y la leemos y le damos permiso
         with open(IMAGE_FILE, 'wb') as file:
             file.write(response.content)
         os.chmod(IMAGE_FILE, 0o755)
@@ -37,6 +42,7 @@ def pokemon_image(pokemon__data):
 
 def stats_pokemon(pokemon_data):
     """Stats Pokemon"""
+    # - Imprimimos estadistica del pokemon
     print(f"name: {pokemon_data['name']}")
     print(f"HP: {pokemon_data['stats'][0]['base_stat']}")
     print(f"type: {pokemon_data['types'][0]['type']['name']}")
@@ -46,6 +52,7 @@ def stats_pokemon(pokemon_data):
 def show_pokemon_ascii(pokemon_data):
     """Magic."""
     try:
+        # usamos ascci magic para dibujar la imagen la terminal
         with Image.open(IMAGE_FILE) as image:
             ascii_magic.from_image(IMAGE_FILE).to_terminal(
                 columns=120, width_ratio=3)
@@ -59,6 +66,7 @@ def show_pokemon_ascii(pokemon_data):
 def check_response_pokemon(res, pokemon_name):
     """validate."""
     if res.status_code != 200:
+        # validamos la peticion
         print(
             f"Oh lo siento el pokemon ingresado no se pudo encontrar:  {pokemon_name}")
         return False
@@ -68,6 +76,7 @@ def check_response_pokemon(res, pokemon_name):
 def show_all_pokemon():
     """Request."""
     response = requests.get(POKEMON, timeout=10)
+    # pedimos el resultado de todos los pokemon
     result = response.json()["results"]
     for pokemon in result:
         print(pokemon["name"])
--- src/main.py
+++ src/main.py
@@ -5,16 +5,22 @@ from api import api
 EXIT = 'exit'
 LIST = 'all'
 
+# manejamos que esto se ejecute como modulo princiapll de la aplicacion
 if __name__ == '__main__':
+    # mantemos un entorno de ejecucion activo
     while True:
         pokemon_name = input("Ingresa el nombre de un pokemon: ").lower().strip()
+        # validamos si el usuario no quiere mantener ese servidor activo
         if pokemon_name == EXIT:
             break
+        # si desea ver toda la lista de los pokemon
         elif pokemon_name == LIST:
           api.show_all_pokemon()
           print()
           continue
+        # aqui hacemos la petición
         response = requests.get(api.URL_API + pokemon_name, timeout=10)
+        # les pasamos la respuesta mas el nombre de la petición
         if api.check_response_pokemon(response, pokemon_name):
             print()
             pokemon_data = api.get_pokemon_data(pokemon_name)
